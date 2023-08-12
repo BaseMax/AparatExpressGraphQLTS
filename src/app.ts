8ed9083f8bd5ed3bd50b0eb7ef3e27a39530e854
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-12 22:23:28 +0330

    create base app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log('Success files0')
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import resolvers from './schema/resolvers' ; 
+import {typeDefs} from './schema/schema' ;
+
+const app = express()
+
+const server = new ApolloServer({
+    resolvers ,
+    typeDefs , 
+})
+
+
+async function startApolloServer() {
+    await server.start();
+  
+    server.applyMiddleware({ app });
+  
+    const PORT = process.env.PORT || 3000;
+  
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+}
+
+
+startApolloServer().catch(err=>{
+    console.error(err)
+})
+
