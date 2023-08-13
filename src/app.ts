aaca9fa3fd20bfb7ecba786b9090d2308562bcd0
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-13 22:05:57 +0330

    add authChecker

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,30 @@
 import "reflect-metadata";
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
-import { buildSchema } from 'type-graphql';
+import { buildSchemaSync } from 'type-graphql';
 import { HelloWorldResolver } from './resolvers/HelloWorldResolver';
 import { ApolloServerPluginInlineTrace } from "apollo-server-core";
 import { config } from "dotenv";
 import mongoose from "mongoose";
 import { UserResolver } from "./resolvers/UserResolver";
+import { authChecker } from "./jwt/authMiddleware";
 
 (async ()=>{
     config()
-
+    
     const app = express()
     const port = process.env.PORT || 3000 ;
 
+    const schema = buildSchemaSync({
+        resolvers : [
+            HelloWorldResolver , 
+            UserResolver 
+        ] , 
+        authChecker 
+    }) ;
+
     const apolloServer = new ApolloServer({
-        schema : await buildSchema({
-            resolvers : [
-                HelloWorldResolver , 
-                UserResolver 
-            ]
-        }) , 
+        schema , 
         context : ({req , res})=>({req , res}) , 
         plugins: [ApolloServerPluginInlineTrace()] , 
     })
