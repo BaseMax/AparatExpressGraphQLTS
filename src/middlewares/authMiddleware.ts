3a82cd3e7204a03df7680102a363814b541f9327
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-14 22:32:02 +0330

    The all middlewares included in this folder

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,31 @@
+import { AuthService } from '../services/AuthService';
+import { UserService } from '../services/UserService';
+import { AuthChecker } from 'type-graphql';
+import { ContextType } from '../interfaces/contextType'; 
+
+const authService = new AuthService();
+const userService = new UserService();
+
+export const authChecker: AuthChecker<ContextType> = async ({ context }) => {
+  const authorization = context.req.headers['authorization'];
+
+  if (!authorization) {
+    return false;
+  }
+
+  const token = authorization.replace('Bearer ', '');
+
+  try {
+    const { sub } = authService.validateToken(token)
+
+    if (typeof sub === 'string') {
+      const user = await userService.findById(sub);
+      context.req.user = user ; 
+    } else {
+      throw new Error('Invalid token');
+    }
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
