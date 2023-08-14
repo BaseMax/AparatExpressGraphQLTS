3b8ac0eed4456a8a1e7b19e44b969c5518a70923
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-14 20:21:37 +0330

    Add feature (hash user password before insert db)

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,11 +1,13 @@
-import { model, Schema } from 'mongoose';
+import { genSalt, genSaltSync, hash, hashSync } from 'bcrypt';
+import mongoose, { model, Schema } from 'mongoose';
 
 enum Role {
     ADMIN="ADMIN" , 
     USER="USER" ,
 }
 
-interface IUser {
+export interface IUser {
+    id : mongoose.Types.ObjectId ; 
     name : string ; 
     email : string ; 
     username : string ; 
@@ -19,7 +21,7 @@ const userSchema = new Schema<IUser>({
     name : {type : String , required : true } , 
     email : {type : String  , required : true , unique : true} ,
     username : { type : String  , required : true , unique : true } , 
-    password : {type : String , required : true , unique : true } , 
+    password : {type : String , required : true} , 
     role : {
         type : String ,
         enum : ['ADMIN','USER'] , 
@@ -27,4 +29,17 @@ const userSchema = new Schema<IUser>({
     } ,
 })
 
-export const User = model<IUser>('User' , userSchema)
+userSchema.pre('save' , function (next) {
+    if(!this.isModified('password')) return next()
+
+    try {
+        const salt = genSaltSync(10);
+        const hashedPassword = hashSync(this.password , salt);
+        this.password = hashedPassword ; 
+        next()
+    } catch (error) {
+        return next(error)
+    }
+})
+
+export const User = model<IUser>('User' , userSchema);
