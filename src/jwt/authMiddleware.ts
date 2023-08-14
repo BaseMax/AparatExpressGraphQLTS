e0f82890f1ebe776c64eb419448a15a34c2e11dc
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-14 20:18:56 +0330

    The all object type

--- src/jwt/authMiddleware.ts
+++ src/jwt/authMiddleware.ts
@@ -1,16 +1,32 @@
-import passport from "passport";
-import { AuthChecker } from "type-graphql";
-import { ContextType } from "../interfaces/contextType";
+import { AuthService } from '../services/AuthService';
+import { UserService } from '../services/UserService';
+import { AuthChecker } from 'type-graphql';
+import { ContextType } from '../interfaces/contextType'; 
+import { verify } from 'jsonwebtoken';
 
+const authService = new AuthService();
+const userService = new UserService();
 
-export const authChecker:AuthChecker<ContextType> = async ({context}) => {
-    return new Promise<boolean>((resolve , reject)=>{
-        passport.authenticate('jwt' , {session : false} , (err , user)=>{
-            if(err || !user){
-                resolve(false)
-            }
-            context.req.user = user ; 
-            resolve(true)
-        })(context.req , context.res)
-    })
-}
+export const authChecker: AuthChecker<ContextType> = async ({ context }) => {
+  const authorization = context.req.headers['authorization'];
+
+  if (!authorization) {
+    return false;
+  }
+
+  const token = authorization.replace('Bearer ', '');
+
+  try {
+    const { sub } = authService.validateToken(token)
+
+    if (typeof sub === 'string') {
+      const user = await userService.findById(sub);
+      context.req.user = user ; 
+    } else {
+      throw new Error('Invalid token');
+    }
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
