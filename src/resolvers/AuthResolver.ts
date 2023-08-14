b01f2350a610d7a8154f517ad6bcb6ca5cfb7707
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-14 20:22:32 +0330

    Add new resolver

---
+++ src/resolvers/AuthResolver.ts
@@ -0,0 +1,29 @@
+import { LoginInput } from "../inputs/loginInput";
+import { RegisterInput } from "../inputs/registerInput";
+import { AuthService } from "../services/AuthService";
+import { Arg , Mutation, Resolver } from "type-graphql";
+import { Auth } from "../object-types/login";
+
+@Resolver(of => Auth)
+export class AuthResolver {
+    private readonly AuthService:AuthService ; 
+
+    constructor(){
+        this.AuthService = new AuthService()
+    }
+
+    @Mutation(()=>Auth)
+    async login(
+        @Arg("loginInput" , ()=>LoginInput) loginInput:LoginInput
+    ){
+        return await this.AuthService.login(loginInput) ;
+    }
+
+
+    @Mutation(()=>Auth)
+    async register(
+        @Arg('registerInput' , ()=>RegisterInput) registerInput:RegisterInput
+    ){
+        return await this.AuthService.register(registerInput) ;
+    }
+}
