241d1dd43531c87acb9de7316f0dd1eece9407cb
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-14 20:23:21 +0330

    Add fist service

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,7 +1,15 @@
+import { HttpError } from "../errors/http-error";
+import { User } from "../models/UserModel";
+import { UserEntity } from "../object-types/user-entity";
 
+export class UserService {
+    async findById(id : string):Promise<UserEntity>{
+        const user = await User.findById(id)
 
-const UserService = {
-    createUser : async ()=>{
-        
+        if(!user){
+            throw new HttpError(400 , 'User is invalid')
+        }
+
+        return user ; 
     }
 }
