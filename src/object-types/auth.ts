378324c8a6fac70c4cf9c7abbc557d896d77b1e6
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-15 15:40:58 +0330

    Change name

---
+++ src/object-types/auth.ts
@@ -0,0 +1,10 @@
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class Auth {
+    @Field(()=>String )
+    access_token : string ; 
+
+    @Field(()=>String)
+    role : string 
+}
