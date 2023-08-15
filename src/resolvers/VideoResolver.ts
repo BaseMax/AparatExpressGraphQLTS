2a5ab32a43d69c2b9c8dce9ba26fc10a190c2c02
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-15 15:43:39 +0330

    Create empty resolver

--- src/resolvers/VideoResolver.ts
+++ src/resolvers/VideoResolver.ts
@@ -1,3 +1,20 @@
+import { FileUpload, GraphQLUpload } from "graphql-upload-ts";
+import { CreateVideoInput } from "../inputs/createVideoInput";
+import { StatusResult } from "../object-types/status-result";
+import { VideoService } from "../services/VideoService";
+import { Arg, Mutation, Resolver  } from "type-graphql";
+
+@Resolver()
 export class VideoResolver {
+
+    private readonly videoService : VideoService ;
     
+    constructor(){
+        this.videoService = new VideoService()
+    }
+
+    @Mutation(()=>String)
+    async uploadFile(@Arg("file", () => GraphQLUpload) file: FileUpload): Promise<FileUpload> {
+        return file ; 
+    }
 }
