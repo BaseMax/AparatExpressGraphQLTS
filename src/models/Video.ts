62850436c5899cf5c52b757b4f5f05317cb19ab5
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-15 15:42:31 +0330

    Add video models

--- src/models/Video.ts
+++ src/models/Video.ts
@@ -1,16 +1,28 @@
-import { Schema, Types } from "mongoose";
+import { model, Schema, Types } from "mongoose";
 
 
 interface IVideo extends Document{
     title : string ; 
+    filePath : string ; 
+    fileUrl : string ; 
     description : string ; 
     category : Types.ObjectId ; 
+    like : Types.Array<Types.ObjectId>
     comments : Types.Array<Types.ObjectId> ; 
 }
 
 
 const videoSchema = new Schema<IVideo>({
-    title : {
-        
-    }
-})
+    title : { type : String , required : true}, 
+    description : { type : String , required : true}, 
+    filePath : {type : String} , 
+    fileUrl : {type : String } ,
+    category : {type : Schema.Types.ObjectId , ref : 'Category'} ,
+    comments : [{type : Schema.Types.ObjectId , ref : 'Comment'}] , 
+    like : [{type : Schema.Types.ObjectId, ref : 'User'}] , 
+});
+
+
+const Video = model<IVideo>("Video" , videoSchema);
+
+export {Video} ;
