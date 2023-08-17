48296d08bd338ddfa3c10f5a1939389d1e0ea423
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-17 21:41:35 +0330

    Create video input

--- src/inputs/createVideoInput.ts
+++ src/inputs/createVideoInput.ts
@@ -3,10 +3,13 @@ import { Field, InputType } from "type-graphql";
 @InputType()
 export class CreateVideoInput {
     @Field(()=>String)
-    filePath : string ;
+    path : string ;
 
     @Field(()=>String)
     title : string  ; 
+
+    @Field(()=>String)
+    url : string ;
 
     @Field(()=>String)
     description : string ;
