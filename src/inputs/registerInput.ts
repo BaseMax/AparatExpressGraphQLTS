55696400946bab7dc458028f232229ca2d3f9199
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-17 21:42:21 +0330

    Add firstName and lastName

--- src/inputs/registerInput.ts
+++ src/inputs/registerInput.ts
@@ -3,7 +3,10 @@ import { Field, InputType } from "type-graphql";
 @InputType()
 export class RegisterInput {
     @Field(()=>String )
-    name : string ; 
+    firstName : string ; 
+
+    @Field(()=>String )
+    lastName : string ; 
     
     @Field(()=>String )
     email : string ; 
