944500d7688b74d93e06c1c18c5f0dd7812c0ad8
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-17 21:40:30 +0330

    Change field

--- src/object-types/entity/video-entity.ts
+++ src/object-types/entity/video-entity.ts
@@ -1,10 +1,12 @@
-import { Types } from "mongoose";
 import { Field, ObjectType } from "type-graphql";
+import { CommentEntity } from "./comment-entity";
+import { UserEntity } from "./user-entity";
+
 
 @ObjectType()
 export class VideoEntity {
-    @Field(()=>Types.ObjectId)
-    id : Types.ObjectId;
+    @Field(()=>String)
+    id : string ;
     
     @Field(()=>String)
     title : string ;
@@ -12,12 +14,21 @@ export class VideoEntity {
     @Field(()=>String)
     description : string ;
 
-    @Field(()=>String )
+    @Field(()=>String)
+    url : string;
+
+    @Field(()=>String)
+    path : string;
+
+    @Field(()=>String) 
+    category : string 
+
+    @Field(()=>UserEntity)
+    author : UserEntity
+
+    @Field(()=>[CommentEntity])
+    comments : CommentEntity[] ;
+
     authorId: string;
-    
-    filePath : string; 
-    fileUrl : string ; 
-    categoryId : Types.ObjectId ; 
-    commentsIds : Types.ObjectId ; 
-    likesIds : Types.ObjectId 
+    categoryId : string ; 
 }
