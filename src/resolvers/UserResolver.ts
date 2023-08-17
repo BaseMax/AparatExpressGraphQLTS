9a68f5a64514997db7240a2e58f8c8f068b3fbca
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-17 21:39:48 +0330

    The change data types

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,16 +1,9 @@
-import { User } from "../models/User";
-import { Arg, Authorized, Ctx, Query, Resolver } from "type-graphql";
+import { Authorized, Ctx, Query, Resolver } from "type-graphql";
 import { UserEntity } from "../object-types/entity/user-entity";
 import { ContextType } from "src/interfaces/contextType";
 
-
 @Resolver()
 export class UserResolver{
-    @Query(()=>[UserEntity])
-    async findAllUsers(){
-        return User.find() 
-    }
-
     @Authorized()
     @Query(()=>UserEntity)
     async me(
