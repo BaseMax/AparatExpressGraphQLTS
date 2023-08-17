3a258fe8ba6a528172f1f10539c18affe90f9096
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-17 21:38:42 +0330

    Remove unknown code and create routes

--- src/app.ts
+++ src/app.ts
@@ -5,25 +5,31 @@ config()
 import "reflect-metadata";
 import './jwt/jwtStrategy' ;
 import express from 'express';
-import mongoose from "mongoose";
 import passport from "passport";
 import { buildSchemaSync } from 'type-graphql';
 import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginInlineTrace } from "apollo-server-core";
-import { UserResolver } from "./resolvers/UserResolver";
 import { authChecker } from "./middlewares/authMiddleware";
+import { ApolloServerPluginInlineTrace } from "apollo-server-core";
 
 
 import { AuthResolver } from "./resolvers/AuthResolver";
 import { VideoResolver } from "./resolvers/VideoResolver";
+import { UserResolver } from "./resolvers/UserResolver";
 import { HelloWorldResolver } from './resolvers/HelloWorldResolver';
+
+
+import router from './routes/index' ; 
+
 
 (async ()=>{
     const app = express()
     const port = process.env.PORT || 3000 ;
 
-
-    app.use(passport.initialize());
+    app.use(express.static('public'))
+    app.use(passport.initialize())
+    app.use(express.urlencoded({extended : true}))
+    app.use(express.json()); 
+    app.use('/' , router)
 
     const schema = buildSchemaSync({
         resolvers : [
@@ -45,11 +51,9 @@ import { HelloWorldResolver } from './resolvers/HelloWorldResolver';
         introspection: true,
     })
 
-
     await apolloServer.start()
-    await mongoose.connect(process.env.MONGODB_URI)
-
     apolloServer.applyMiddleware({app , cors : false});
+
     app.listen(port , ()=>{
         console.clear();
         console.log(process.version);
