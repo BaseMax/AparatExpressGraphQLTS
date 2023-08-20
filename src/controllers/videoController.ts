73cd0ab3bca81f1a13334f5e5ddb35e894047328
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-20 23:44:53 +0330

    Clean unknown code

--- src/controllers/videoController.ts
+++ src/controllers/videoController.ts
@@ -1,8 +1,6 @@
-import { StatusResult } from "../object-types/status-result";
 import { Request , Response } from 'express'
 import { VideoService } from "../services/VideoService";
 import { CreateVideoInput } from "../inputs/createVideoInput";
-import uuid from 'uuid' ;
 
 const videoService = new VideoService()
 
@@ -10,15 +8,15 @@ export async function createVideo(req:Request,res:Response):Promise<any> {
     const {
         title, 
         description ,
+        tags , 
     } = req.body;
-
-    console.log()
 
     const data:CreateVideoInput = {
         title : title , 
         description : description , 
         path : req.file.path , 
         url : `${req.hostname}/${req.file.path}`, 
+        tags ,
     }
 
     const result = await videoService.create(data , req.user)
