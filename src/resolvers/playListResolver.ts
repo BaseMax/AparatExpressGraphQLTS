77069a7be029d6bfd36cfd1e74250042a9f24f9e
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-20 23:46:15 +0330

    Add play list resolvers

--- src/resolvers/playListResolver.ts
+++
@@ -1,9 +0,0 @@
-export class PlayListResolver {
-    findOnePlaylist(){}
-    listPlaylists(){}
-    createPlaylist(){}
-    updatePlaylist(){}
-    deletePlaylist(){}
-    addVideoToPlaylist(){}
-    removeVideoFromPlaylist(){}
-}
