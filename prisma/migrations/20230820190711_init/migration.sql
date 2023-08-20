0c1e7d69cf63256925be6903e76b412d3ac8c218
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-20 23:41:56 +0330

    Build a new migration

--- prisma/migrations/20230820190711_init/migration.sql
+++ prisma/migrations/20230820190711_init/migration.sql
@@ -21,10 +21,20 @@ CREATE TABLE "Video" (
     "description" TEXT NOT NULL,
     "url" TEXT NOT NULL,
     "path" TEXT NOT NULL,
-    "categoryId" TEXT,
+    "tags" TEXT[],
     "authorId" TEXT NOT NULL,
+    "categoryId" TEXT,
+    "playListId" TEXT,
 
     CONSTRAINT "Video_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PlayList" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "PlayList_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -51,10 +61,13 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- AddForeignKey
+ALTER TABLE "Video" ADD CONSTRAINT "Video_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Video" ADD CONSTRAINT "Video_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Video" ADD CONSTRAINT "Video_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Video" ADD CONSTRAINT "Video_playListId_fkey" FOREIGN KEY ("playListId") REFERENCES "PlayList"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Comment" ADD CONSTRAINT "Comment_videoId_fkey" FOREIGN KEY ("videoId") REFERENCES "Video"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
